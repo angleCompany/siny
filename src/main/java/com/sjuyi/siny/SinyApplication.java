740aaa4099baa5bfda7caf8bb30f4a5d09c694c8
Edgar <firecode@kakao.com>
2025-01-23 00:03:36 +0900

    siny init

---
+++ src/main/java/com/sjuyi/siny/SinyApplication.java
@@ -0,0 +1,13 @@
+package com.sjuyi.siny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinyApplication.class, args);
+    }
+
+}
