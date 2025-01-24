6a9e2678a6b7d80e23af3c658086937454158600
Edgar <firecode@kakao.com>
2025-01-24 23:02:24 +0900

    test

--- src/main/java/com/sjuyi/siny/SinyApplication.java
+++ src/main/java/com/sjuyi/siny/SinyApplication.java
@@ -8,6 +8,7 @@ public class SinyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SinyApplication.class, args);
+        System.out.println("start");
     }
 
 }
