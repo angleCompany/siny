8086841937b723b561cac32194b18ef64613c645
Edgar <firecode@kakao.com>
2025-01-24 23:03:50 +0900

    restore

--- src/main/java/com/sjuyi/siny/SinyApplication.java
+++ src/main/java/com/sjuyi/siny/SinyApplication.java
@@ -8,7 +8,6 @@ public class SinyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SinyApplication.class, args);
-        System.out.println("start");
     }
 
 }
